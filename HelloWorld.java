0989f0dd7691a0e01312922d15da751e57d0099e
VenkateshMuthangi <114325414+VenkateshMuthangi@users.noreply.github.com>
2024-02-13 10:13:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,3 @@
-/** 
- * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
- */
 public class HelloWorld {
   public static void main(String[] args) {
      int num1 = 55, num2 = 15, sum,sub;
